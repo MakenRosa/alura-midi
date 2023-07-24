99648d247138b9e49d66016ffc00b4890fdbf353
makenrosa <maken.cristhian@gmail.com>
2023-07-24 17:32:56 -0300

    feat: added space and enter key press

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const buttons = document.querySelectorAll('.key');
-    buttons.forEach(button => {
-        const sound = document.getElementById(`sound_${button.classList[1]}`);
-        button.addEventListener('click', () => {
-            sound.currentTime = 0;
-            sound.play();
-        });
+buttons.forEach(button => {
+    const sound = document.getElementById(`sound_${button.classList[1]}`);
+    if (!sound) {
+        return;
+    }
+    button.addEventListener('click', () => {
+        sound.currentTime = 0;
+        sound.play();
     });
+    button.addEventListener('keydown', (event) => {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            button.classList.add('active');
+        }
+    });
+    button.addEventListener('keyup', (event) => {
+        if (event.code === 'Space' || event.code === 'Enter') {
+            button.classList.remove('active');
+        }
+    });
+});
